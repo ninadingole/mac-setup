9dd95249a5fbd8ccfda4f9eb6b521935d366d5d1
Neenad Ingole <needna78@gmail.com>
2021-05-06 16:04:46 +0530

    do not reinstall omf and fisher if exists

--- setup-fish.sh
+++ setup-fish.sh
@@ -8,21 +8,30 @@ OMF_CONFIG=~/.config/omf
 function setup_fisher {
     info "Setting fisher plugin manager"
     
-    curl -sL "https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish" > $FISH_CONFIG/functions/fisher.fish
-    curl -sL "https://raw.githubusercontent.com/jorgebucaran/fisher/main/completions/fisher.fish" > $FISH_CONFIG/completions/fisher.fish
+    if [ -e $FISH_CONFIG/functions/fisher.fish ] && [ -e $FISH_CONFIG/completions/fisher.fish ]; then
+        warn "fisher already exists"
+    else
+        curl -sL "https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish" > $FISH_CONFIG/functions/fisher.fish
+        curl -sL "https://raw.githubusercontent.com/jorgebucaran/fisher/main/completions/fisher.fish" > $FISH_CONFIG/completions/fisher.fish
+    fi
 }
 
 function setup_omf {
     info "Setting OMF manager"
-    curl -L https://get.oh-my.fish | fish
-    fish -c "omf update"
     
-    info "Setting up Bundles and themes"
-    
-    ln -s $PWD/fish/omf/bundle $OMF_CONFIG/bundle
-    ln -s $PWD/fish/omf/theme $OMF_CONFIG/theme
-    
-    fish -c "omf update"
+    if fish -c "omf version"; then
+        warn "OMF already exists"
+    else
+        curl -L https://get.oh-my.fish | fish
+        fish -c "omf update"
+        
+        info "Setting up Bundles and themes"
+        
+        ln -s $PWD/fish/omf/bundle $OMF_CONFIG/bundle
+        ln -s $PWD/fish/omf/theme $OMF_CONFIG/theme
+        
+        fish -c "omf update"
+    fi
 }
 
 function setup_paths {
