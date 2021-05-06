0721dad3521d69347789f83e4eaba19a8a96cca5
Neenad Ingole <needna78@gmail.com>
2021-05-06 15:49:55 +0530

    Fix formatting

--- fish/alias.fish
+++ fish/alias.fish
@@ -1,3 +1,3 @@
-alias ls="exa --icons"
-alias ll="exa --long --icons"
-alias gitls="exa --long --header --git --icons"
+alias ls 'exa --icons'
+alias ll 'exa --long --icons'
+alias gitls 'exa --long --header --git --icons'
