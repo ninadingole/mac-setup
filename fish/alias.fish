3de8259897748aacb7cf09d3211d5a880af0dbab
Neenad Ingole <needna78@gmail.com>
2022-01-04 08:51:28 +0100

    add exa aliases for fish and zsh

--- fish/alias.fish
+++ fish/alias.fish
@@ -1,6 +1,15 @@
-alias ls='exa --icons'
-alias ll='exa --long --icons'
-alias gitls='exa --long --header --git --icons'
+# general use
+alias ls='exa' # ls
+alias l='exa -lbF --git' # list, size, type, git
+alias ll='exa -lbGF --git' # long list
+alias llm='exa -lbGd --git --sort=modified' # long list, modified date sort
+alias la='exa -lbhHigUmuSa --time-style=long-iso --git --color-scale' # all list
+alias lx='exa -lbhHigUmuSa@ --time-style=long-iso --git --color-scale' # all + extended list
+
+# specialty views
+alias lS='exa -1' # one column, just names
+alias lt='exa --tree --level=2' # tree
+
 
 ## 1password cli
 alias 1passlogin='eval (op signin my)'
